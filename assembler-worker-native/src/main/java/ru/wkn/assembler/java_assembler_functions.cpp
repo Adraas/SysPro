9f318097a87f5fabf5c5734bc5b2231be4c225b4
Adraas <pickalov.artyom@yandex.ru>
2019-04-17 09:12:33 +0400

    feature: add format for functions
    The format for functions into .cpp file was added

---
+++ assembler-worker-native/src/main/java/ru/wkn/assembler/java_assembler_functions.cpp
@@ -0,0 +1,11 @@
+#include <string.h>
+
+JNIEXPORT jstring JNICALL Java_ru_wkn_assembler_AssemblerFunctions_dividingFunction (
+    JNIEnv * jenv, jobject jobj, jstring jstr)
+{
+}
+
+JNIEXPORT jstring JNICALL Java_ru_wkn_assembler_AssemblerFunctions_xorFunction (
+    JNIEnv * jenv, jobject jobj, jstring jstr)
+{
+}
