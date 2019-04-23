8f1d15ba62568ca1cce87b535a22bde96e4e35f1
Adraas <pickalov.artyom@yandex.ru>
2019-04-23 16:00:10 +0400

    docs: create docs for JNI module

--- assembler-worker/src/main/java/ru/wkn/javasm/AssemblerFunctions.java
+++ assembler-worker/src/main/java/ru/wkn/javasm/AssemblerFunctions.java
@@ -1,10 +1,27 @@
 package ru.wkn.javasm;
 
+/**
+ * Class {@code AssemblerFunctions} represent invocation for assembler functions by means of JNI.
+ *
+ * @author Alexey Konev
+ */
 public class AssemblerFunctions implements IAssemblerFunctions {
 
+    /**
+     * Method for the dividing by means of JNI.
+     *
+     * @param commandLine - line with separator into first position and following values
+     * @return result {@code double} of this operation at the {@code String}
+     */
     @Override
     public native String dividingFunction(String commandLine);
 
+    /**
+     * Method for the XOR operation by means of JNI.
+     *
+     * @param commandLine - line with separator into first position and following values
+     * @return result {@code double} of this operation at the {@code String}
+     */
     @Override
     public native String xorFunction(String commandLine);
 }
--- assembler-worker/src/main/java/ru/wkn/javasm/IAssemblerFunctions.java
+++ assembler-worker/src/main/java/ru/wkn/javasm/IAssemblerFunctions.java
@@ -1,8 +1,26 @@
 package ru.wkn.javasm;
 
+/**
+ * Interface {@code IAssemblerFunctions} represent invocation for assembler functions.
+ * Methods receive parameters at the single line of {@code String} with separator into first position of this line.
+ *
+ * @author Alexey Konev
+ */
 public interface IAssemblerFunctions {
 
+    /**
+     * Method for the dividing.
+     *
+     * @param commandLine - line with separator into first position and following values
+     * @return result {@code double} of this operation at the {@code String}
+     */
     String dividingFunction(String commandLine);
 
+    /**
+     * Method for the XOR operation.
+     *
+     * @param commandLine - line with separator into first position and following values
+     * @return result {@code double} of this operation at the {@code String}
+     */
     String xorFunction(String commandLine);
 }
