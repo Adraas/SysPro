9f7dc7fda56e72ff28901b54de04c6ca0c541bcc
Adraas <pickalov.artyom@yandex.ru>
2019-04-23 15:28:31 +0400

    feature: finish semantic analyzer
    The semantic analyzer was finished.

--- expression-analyzer/src/main/java/ru/wkn/analyzers/syntax/semantics/CSharpeSemanticsAnalyzer.java
+++ expression-analyzer/src/main/java/ru/wkn/analyzers/syntax/semantics/CSharpeSemanticsAnalyzer.java
@@ -1,35 +1,43 @@
 package ru.wkn.analyzers.syntax.semantics;
 
+import java.util.regex.Pattern;
+
 public class CSharpeSemanticsAnalyzer implements ISemanticsAnalyzer {
 
     @Override
     public boolean isByteValueCorrect(String byteValueAsString) {
-        return false;
+        double value = Byte.valueOf(byteValueAsString);
+        return value <= Byte.MAX_VALUE && value >= Byte.MIN_VALUE;
     }
 
     @Override
     public boolean isShortValueCorrect(String shortValueAsString) {
-        return false;
+        double value = Short.valueOf(shortValueAsString);
+        return value <= Short.MAX_VALUE && value >= Short.MIN_VALUE;
     }
 
     @Override
     public boolean isIntegerValueCorrect(String integerValueAsString) {
-        return false;
+        double value = Integer.valueOf(integerValueAsString);
+        return value <= Integer.MAX_VALUE && value >= Integer.MIN_VALUE;
     }
 
     @Override
     public boolean isLongValueCorrect(String longValueAsString) {
-        return false;
+        double value = Long.valueOf(longValueAsString);
+        return value <= Long.MAX_VALUE && value >= Long.MIN_VALUE;
     }
 
     @Override
     public boolean isFloatValueCorrect(String floatValueAsString) {
-        return false;
+        double value = Float.valueOf(floatValueAsString);
+        return value <= Float.MAX_VALUE && value >= Float.MIN_VALUE;
     }
 
     @Override
     public boolean isDoubleValueCorrect(String doubleValueAsString) {
-        return false;
+        double value = Double.valueOf(doubleValueAsString);
+        return value <= Double.MAX_VALUE && value >= Double.MIN_VALUE;
     }
 
     @Override
@@ -39,16 +47,24 @@ public class CSharpeSemanticsAnalyzer implements ISemanticsAnalyzer {
 
     @Override
     public boolean isBooleanValueCorrect(String booleanValueAsString) {
-        return false;
+        return booleanValueAsString.equals("true") || booleanValueAsString.equals("false");
     }
 
     @Override
     public boolean isStringValueCorrect(String stringValueAsString) {
-        return false;
+        return stringValueAsString.startsWith("\"")
+                && stringValueAsString.endsWith("\"");
     }
 
     @Override
     public boolean isVariableNameCorrect(String variableAsString) {
-        return false;
+        String allSpacesRegex = "[\\s\\n\\r\\t]*";
+        String serviceCharactersRegex = "\\(\\)\\{\\{\\+=-\\[\\]\\|\\\\/&\\?\\^%#@\\$\\*:;\"\'!\\.";
+        String anyNameCharSequenceRegex = "(_*[A-Za-z]+.*(^"
+                .concat(allSpacesRegex)
+                .concat(")([^")
+                .concat(serviceCharactersRegex)
+                .concat("])");
+        return Pattern.compile(anyNameCharSequenceRegex).matcher(variableAsString).lookingAt();
     }
 }
