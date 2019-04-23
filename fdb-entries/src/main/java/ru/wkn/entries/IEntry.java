0f29aa3033f73421ad29702ff3708560e2f2c909
Adraas <pickalov.artyom@yandex.ru>
2019-04-23 16:00:31 +0400

    docs: start docs for fdb-entries module

--- fdb-entries/src/main/java/ru/wkn/entries/IEntry.java
+++ fdb-entries/src/main/java/ru/wkn/entries/IEntry.java
@@ -1,6 +1,16 @@
 package ru.wkn.entries;
 
+/**
+ * Interface {@code IEntry} represent entry entity for file-worker and DB-worker.
+ *
+ * @author Artem Pikalov
+ */
 public interface IEntry {
 
+    /**
+     * Method for the representation {@code this} entry as single {@code String} line.
+     *
+     * @return {@code String} line with main information
+     */
     String singleLineRecording();
 }
