ff558cc0be4f9b06ff340bc019946e7ea296e8c8
Adraas <pickalov.artyom@yandex.ru>
2019-04-24 10:01:22 +0400

    refactor(docs): refactor docs

--- assembler-worker/src/main/java/ru/wkn/javasm/AssemblerFunctions.java
+++ assembler-worker/src/main/java/ru/wkn/javasm/AssemblerFunctions.java
@@ -3,6 +3,7 @@ package ru.wkn.javasm;
 /**
  * Class {@code AssemblerFunctions} represent invocation for assembler functions by means of JNI.
  *
+ * @see IAssemblerFunctions
  * @author Alexey Konev
  */
 public class AssemblerFunctions implements IAssemblerFunctions {
@@ -10,9 +11,7 @@ public class AssemblerFunctions implements IAssemblerFunctions {
     /**
      * Method for the dividing by means of JNI.
      *
-     * @param dividend - dividend for dividing function
-     * @param divisor - divisor for dividing function
-     * @return result {@code double} of this operation
+     * @see IAssemblerFunctions#dividingFunction(double, double)
      */
     @Override
     public native double dividingFunction(double dividend, double divisor);
@@ -20,9 +19,7 @@ public class AssemblerFunctions implements IAssemblerFunctions {
     /**
      * Method for the XOR operation by means of JNI.
      *
-     * @param a - first value for XOR operation
-     * @param b - second value for XOR operation
-     * @return result {@code double} of this operation
+     * @see IAssemblerFunctions#xorFunction(double, double)
      */
     @Override
     public native double xorFunction(double a, double b);
