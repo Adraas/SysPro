40d86f2b52a617903146ab701f106987418eb5c1
Adraas <pickalov.artyom@yandex.ru>
2019-04-24 09:28:21 +0400

    refactor: replace cpp-file to c-file

---
+++ cpp-native/src/main/java/ru/wkn/cpp/java_assembler_functions.c
@@ -0,0 +1,22 @@
+#include "ru_wkn_javasm_AssemblerFunctions.h"
+#include <string.h>
+
+/*
+ * Class:     ru_wkn_javasm_AssemblerFunctions
+ * Method:    dividingFunction
+ * Signature: (DD)D
+ */
+JNIEXPORT jdouble JNICALL Java_ru_wkn_javasm_AssemblerFunctions_dividingFunction
+  (JNIEnv *, jobject, jdouble, jdouble)
+{
+}
+
+/*
+ * Class:     ru_wkn_javasm_AssemblerFunctions
+ * Method:    xorFunction
+ * Signature: (DD)D
+ */
+JNIEXPORT jdouble JNICALL Java_ru_wkn_javasm_AssemblerFunctions_xorFunction
+  (JNIEnv *, jobject, jdouble, jdouble)
+{
+}
