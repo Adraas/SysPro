2e0da71b774c8f5c697aa9143dee949695f6332e
Adraas <pickalov.artyom@yandex.ru>
2019-05-14 12:09:41 +0400

    feature: new window types

--- sys-pro-launcher/src/main/java/ru/wkn/views/WindowType.java
+++ sys-pro-launcher/src/main/java/ru/wkn/views/WindowType.java
@@ -1,4 +1,11 @@
 package ru.wkn.views;
 
 public enum WindowType {
+
+    MAIN_WINDOW,
+    ANALYZER_WINDOW,
+    ASSEMBLER_MATH_WINDOW,
+    FILE_DB_WINDOW,
+    CSV_ENTRY_WINDOW,
+    PLAIN_TEXT_ENTRY_WINDOW
 }
