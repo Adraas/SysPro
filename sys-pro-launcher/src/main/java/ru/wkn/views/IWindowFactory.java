9757b3fc217621751936a17634026d3e29244f9a
Adraas <pickalov.artyom@yandex.ru>
2019-05-15 09:17:09 +0400

    refactor: add custom exception

--- sys-pro-launcher/src/main/java/ru/wkn/views/IWindowFactory.java
+++ sys-pro-launcher/src/main/java/ru/wkn/views/IWindowFactory.java
@@ -1,6 +1,8 @@
 package ru.wkn.views;
 
+import ru.wkn.exceptions.WindowTypeException;
+
 public interface IWindowFactory {
 
-    IWindow createWindow(WindowType windowType);
+    IWindow createWindow(WindowType windowType) throws WindowTypeException;
 }
