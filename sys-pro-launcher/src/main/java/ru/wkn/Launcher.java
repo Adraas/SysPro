96dba0e7dc993b74d690995f528e160a160f9005
Adraas <pickalov.artyom@yandex.ru>
2019-05-15 08:58:28 +0400

    feature: add start window's calling

--- sys-pro-launcher/src/main/java/ru/wkn/Launcher.java
+++ sys-pro-launcher/src/main/java/ru/wkn/Launcher.java
@@ -2,6 +2,13 @@ package ru.wkn;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import ru.wkn.views.IWindow;
+import ru.wkn.views.IWindowFactory;
+import ru.wkn.views.WindowFactory;
+import ru.wkn.views.WindowRepository;
+import ru.wkn.views.WindowType;
+
+import java.io.IOException;
 
 public class Launcher extends Application {
 
@@ -10,6 +17,14 @@ public class Launcher extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
+        IWindowFactory windowFactory = new WindowFactory();
+        WindowRepository windowRepository = new WindowRepository(windowFactory);
+        IWindow IWindow = windowRepository.addWindow(WindowType.MAIN_WINDOW);
+        try {
+            IWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
