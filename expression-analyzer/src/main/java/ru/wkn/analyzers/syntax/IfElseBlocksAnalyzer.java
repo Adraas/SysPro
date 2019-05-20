e419e130d7cf4983222dbfd7326923b78cd00712
Adraas <pickalov.artyom@yandex.ru>
2019-05-20 10:32:29 +0400

    feature: create base patterns for if/else block

--- expression-analyzer/src/main/java/ru/wkn/analyzers/syntax/IfElseBlocksAnalyzer.java
+++ expression-analyzer/src/main/java/ru/wkn/analyzers/syntax/IfElseBlocksAnalyzer.java
@@ -5,6 +5,19 @@ import ru.wkn.analyzers.syntax.semantics.ISemanticsAnalyzer;
 
 public class IfElseBlocksAnalyzer extends ExpressionAnalyzer {
 
+    private final String singleBooleanExpressionRegex =
+            "^\\s*([0-9a-zA-Z\\)\\(\\+\\-\\/\\*]*)\\s*(==|!=|>=|<=|<|>)\\s*([0-9a-zA-Z\\)\\(\\+\\-\\/\\*]*)\\s*";
+    private final String variableDeclarationAndAssignmentRegex =
+            "\\s*([a-zA-Z][0-9a-zA-Z]*)\\s*(=|\\+=|\\-=|\\*=|\\/=)\\s*([0-9a-zA-z\\-\\+\\*\\/\\s\\(\\)]*)\\s*;";
+    private final String variableDeclarationRegex = "([a-zA-Z][0-9a-zA-Z]*)\\s*;";
+    private final String variableAssignmentRegex =
+            "^\\s*([a-zA-Z][0-9a-zA-Z]*)\\s*(=|\\+=|\\-=|\\*=|\\/=)\\s*([0-9a-zA-z\\-\\+\\*\\/\\s\\(\\)]*)\\s*;";
+    private final String emptyInstructionRegex = "^\\s*";
+    private final String variableNameRegex = "([a-zA-Z][0-9a-zA-Z]*)";
+    private final String expressionRegex = "";
+    private final String ifBlockRegex =
+            "^\\s*if\\s*\\(([^\\(\\)]*)\\)\\s*{([^{}]*)}\\s*(else\\s*{([^{})]*)})?";
+
     public IfElseBlocksAnalyzer(ISemanticsAnalyzer iSemanticsAnalyzer, boolean isSemanticsAnalyzerActivated) {
         super(iSemanticsAnalyzer, isSemanticsAnalyzerActivated);
     }
