331a9554ff6d2a1d868337a60f7f529b06eceb88
Adraas <pickalov.artyom@yandex.ru>
2019-05-20 21:06:00 +0400

    feature: implement facade

--- expression-analyzer/src/main/java/ru/wkn/ExpressionAnalyzerFacade.java
+++ expression-analyzer/src/main/java/ru/wkn/ExpressionAnalyzerFacade.java
@@ -1,4 +1,14 @@
 package ru.wkn;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import ru.wkn.analyzers.IExpressionAnalyzerFactory;
+import ru.wkn.analyzers.syntax.ExpressionAnalyzer;
+
+@AllArgsConstructor
+@Getter
 public class ExpressionAnalyzerFacade {
+
+    private ExpressionAnalyzer expressionAnalyzer;
+    private IExpressionAnalyzerFactory expressionAnalyzerFactory;
 }
