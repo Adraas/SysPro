76219fda4f5fdff85ad7d730fcc92186d04bce28
Adraas <pickalov.artyom@yandex.ru>
2019-05-20 14:56:40 +0400

    feature: new error message

--- expression-analyzer/src/main/java/ru/wkn/analyzers/exceptions/messages/ErrorMessages.java
+++ expression-analyzer/src/main/java/ru/wkn/analyzers/exceptions/messages/ErrorMessages.java
@@ -11,7 +11,8 @@ public enum ErrorMessages {
     LINE_ERROR("cycle's body is incorrect after line "),
     ACTION_TYPE_ERROR("unknown action type after line "),
     DATA_TYPE_ERROR("unknown data type after line "),
-    VARIABLE_DECLARATION_ERROR("variable declaration is incorrect after line ");
+    VARIABLE_DECLARATION_ERROR("variable declaration is incorrect after line "),
+    INITIALIZATION_ERROR("variable initialization is incorrect after line ");
 
     private String errorMessage;
 }
