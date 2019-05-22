ae7fe9d7f28d2ff9a57b674cd5b410f7dcd2923a
Adraas <pickalov.artyom@yandex.ru>
2019-05-22 07:47:20 +0400

    docs: finish docs for fdb-entries module

--- fdb-entries/src/main/java/ru/wkn/entries/exceptions/EntryException.java
+++ fdb-entries/src/main/java/ru/wkn/entries/exceptions/EntryException.java
@@ -1,5 +1,9 @@
 package ru.wkn.entries.exceptions;
 
+/**
+ * Class {@code EntryException} represents exception for custom specific conditions
+ * for entries.
+ */
 public class EntryException extends Exception {
 
     public EntryException(String message) {
