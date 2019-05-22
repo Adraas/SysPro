96d6a98a6b01b2773f13793be79cac84dd9ad2e8
Adraas <pickalov.artyom@yandex.ru>
2019-05-22 08:20:22 +0400

    docs: start documentation for the expression analyzer module

--- expression-analyzer/src/main/java/ru/wkn/ExpressionAnalyzerFacade.java
+++ expression-analyzer/src/main/java/ru/wkn/ExpressionAnalyzerFacade.java
@@ -5,10 +5,22 @@ import lombok.Getter;
 import ru.wkn.analyzers.IExpressionAnalyzerFactory;
 import ru.wkn.analyzers.syntax.ExpressionAnalyzer;
 
+/**
+ * The class {@code ExpressionAnalyzerFacade} represents the facade design pattern for expression analyzer.
+ *
+ * @author Artem Pikalov
+ */
 @AllArgsConstructor
 @Getter
 public class ExpressionAnalyzerFacade {
 
+    /**
+     * {@code ExpressionAnalyzer} object for expression analyzing.
+     */
     private ExpressionAnalyzer expressionAnalyzer;
+
+    /**
+     * {@code IExpressionAnalyzerFactory} object for analyzer creating.
+     */
     private IExpressionAnalyzerFactory expressionAnalyzerFactory;
 }
--- expression-analyzer/src/main/java/ru/wkn/analyzers/IExpressionAnalyzerFactory.java
+++ expression-analyzer/src/main/java/ru/wkn/analyzers/IExpressionAnalyzerFactory.java
@@ -5,8 +5,24 @@ import ru.wkn.analyzers.exceptions.LanguageException;
 import ru.wkn.analyzers.syntax.ExpressionAnalyzer;
 import ru.wkn.analyzers.syntax.util.Language;
 
+/**
+ * The interface {@code IExpressionAnalyzerFactory} contains factory method for the {@code ExpressionAnalyzer}
+ * objects creating.
+ *
+ * @author Artem Pikalov
+ */
 public interface IExpressionAnalyzerFactory {
 
+    /**
+     * The abstract factory method for the {@code ExpressionAnalyzer} object creating.
+     *
+     * @param expressionAnalyzerType type of the expression analyzer as enum value
+     * @param language programming language for analyzing as enum value
+     * @param isSemanticsActivated will whether the code semantics check be activated
+     * @return {@code ExpressionAnalyzer} object
+     * @throws LanguageException thrown if some problems with indicated language type
+     * @throws AnalyzerException thrown if some problems with indicated analyzer type
+     */
     ExpressionAnalyzer createExpressionAnalyzer(ExpressionAnalyzerType expressionAnalyzerType, Language language,
                                                 boolean isSemanticsActivated) throws LanguageException, AnalyzerException;
 }
