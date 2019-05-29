580195865096044e0b023d8a621a6383129cd3ba
Adraas <pickalov.artyom@yandex.ru>
2019-05-29 07:46:25 +0400

    feature: delete unnecessary exception

--- sys-pro-launcher/src/main/java/ru/wkn/exceptions/ControllerException.java
+++
@@ -1,8 +0,0 @@
-package ru.wkn.exceptions;
-
-public class ControllerException extends Exception {
-
-    public ControllerException(String message) {
-        super("Controller exception cause: ".concat(message));
-    }
-}
