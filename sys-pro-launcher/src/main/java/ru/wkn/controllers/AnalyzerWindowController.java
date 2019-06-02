8e54061767d5473e679195e234e6ad6edc109199
Adraas <pickalov.artyom@yandex.ru>
2019-06-02 20:01:01 +0400

    feature: implements controllers The remaining controllers was implemented.

--- sys-pro-launcher/src/main/java/ru/wkn/controllers/AnalyzerWindowController.java
+++ sys-pro-launcher/src/main/java/ru/wkn/controllers/AnalyzerWindowController.java
@@ -1,19 +1,99 @@
 package ru.wkn.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
+import ru.wkn.ExpressionAnalyzerFacade;
+import ru.wkn.analyzers.ExpressionAnalyzerFactory;
+import ru.wkn.analyzers.ExpressionAnalyzerType;
+import ru.wkn.analyzers.IExpressionAnalyzerFactory;
+import ru.wkn.analyzers.exceptions.AnalyzerException;
+import ru.wkn.analyzers.exceptions.ExpressionException;
+import ru.wkn.analyzers.exceptions.LanguageException;
+import ru.wkn.analyzers.exceptions.SemanticsException;
+import ru.wkn.analyzers.syntax.ExpressionAnalyzer;
+import ru.wkn.analyzers.syntax.util.Language;
 
+// TODO: add choice language and construction types
 public class AnalyzerWindowController extends Controller {
 
     @FXML
-    private TextArea inputArea;
+    private ChoiceBox<String> choiceBoxVariants;
     @FXML
-    private TextArea messageLogArea;
+    private TextArea inputExpressionField;
     @FXML
-    private ChoiceBox variantChoseBox;
+    private CheckBox isSemanticsActivatedCheckBox;
+    @FXML
+    private TextArea expressionAnalyzerMessageLogArea;
+
+    private IExpressionAnalyzerFactory expressionAnalyzerFactory;
+    private ExpressionAnalyzerFacade expressionAnalyzerFacade;
+
+    public void initialize() {
+        initChoiceBox();
+        initChoiceBoxEventHandlers();
+        initCheckBoxEventHandlers();
+        expressionAnalyzerFactory = new ExpressionAnalyzerFactory();
+    }
+
+    private void initChoiceBox() {
+        String[] variantItems = new String[]{"Cycle \"while\" with precondition"};
+        ObservableList<String> variants = FXCollections.observableArrayList(variantItems);
+        choiceBoxVariants.setItems(variants);
+        choiceBoxVariants.setValue(variantItems[0]);
+    }
+
+    private void initChoiceBoxEventHandlers() {
+        ChangeListener<String> changeListener = (observable, oldValue, newValue) -> {
+            String variant = choiceBoxVariants.getValue();
+            if (variant.equals(choiceBoxVariants.getItems().get(0))) {
+                try {
+                    ExpressionAnalyzer expressionAnalyzer = expressionAnalyzerFactory
+                            .createExpressionAnalyzer(ExpressionAnalyzerType.CYCLE_WHILE_WITH_PRECONDITION,
+                                    Language.C_SHARPE, isSemanticsActivatedCheckBox.isSelected());
+                    expressionAnalyzerFacade = new ExpressionAnalyzerFacade(expressionAnalyzer,
+                            expressionAnalyzerFactory);
+                } catch (LanguageException | AnalyzerException e) {
+                    informAboutExceptionCause(e);
+                }
+            }
+        };
+        choiceBoxVariants.getSelectionModel().selectedItemProperty().addListener(changeListener);
+    }
+
+    private void initCheckBoxEventHandlers() {
+        ChangeListener<Boolean> changeListener = ((observable, oldValue, newValue) -> {
+            Boolean isSemanticsActivated = isSemanticsActivatedCheckBox.isSelected();
+            expressionAnalyzerFacade.getExpressionAnalyzer().setSemanticsAnalyzerActivated(isSemanticsActivated);
+        });
+        isSemanticsActivatedCheckBox.selectedProperty().addListener(changeListener);
+    }
 
     @FXML
     private void onClickRun() {
+        String variant = choiceBoxVariants.getValue();
+        if (variant.equals(choiceBoxVariants.getItems().get(0))) {
+            try {
+                expressionAnalyzerMessageLogArea.setText(expressionAnalyzerMessageLogArea.getText()
+                        .concat("\nExpression is correct: ")
+                        .concat(String.valueOf(expressionAnalyzerFacade
+                                .getExpressionAnalyzer().isSyntaxCorrect(inputExpressionField.getText()))));
+            } catch (ExpressionException | SemanticsException e) {
+                informAboutExceptionCause(e);
+            }
+        }
+    }
+
+    private void informAboutExceptionCause(Exception e) {
+        e.printStackTrace();
+        String message = e.getMessage();
+        expressionAnalyzerMessageLogArea.setText(expressionAnalyzerMessageLogArea.getText()
+                .concat("\n").concat(message));
+        showInformation(e.getClass().getSimpleName(), message, Alert.AlertType.ERROR);
     }
 }
--- sys-pro-launcher/src/main/java/ru/wkn/controllers/AssemblerMathWindowController.java
+++ sys-pro-launcher/src/main/java/ru/wkn/controllers/AssemblerMathWindowController.java
@@ -1,22 +1,66 @@
 package ru.wkn.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import ru.wkn.jni.AssemblerMath;
+import ru.wkn.jni.functions.AssemblerFunctions;
+import ru.wkn.jni.functions.IAssemblerFunctions;
 
 public class AssemblerMathWindowController extends Controller {
 
     @FXML
-    private ChoiceBox assemblerVariantChoiceBox;
+    private ChoiceBox<String> choiceBoxVariants;
     @FXML
     private TextField inputAField;
     @FXML
     private TextField inputBField;
     @FXML
-    private TextArea assemblerMessageLog;
+    private TextArea assemblerMessageLogArea;
+
+    private AssemblerMath assemblerMath;
+
+    public void initialize() {
+        initChoiceBox();
+        IAssemblerFunctions assemblerFunctions = new AssemblerFunctions();
+        assemblerMath = new AssemblerMath(assemblerFunctions);
+    }
+
+    private void initChoiceBox() {
+        String[] variantItems = new String[]{"Dividing", "XOR"};
+        ObservableList<String> variants = FXCollections.observableArrayList(variantItems);
+        choiceBoxVariants.setItems(variants);
+        choiceBoxVariants.setValue(variantItems[0]);
+    }
 
     @FXML
     private void onClickRun() {
+        String variant = choiceBoxVariants.getValue();
+        try {
+            if (variant.equals(choiceBoxVariants.getItems().get(0))) {
+                double a = Double.parseDouble(inputAField.getText());
+                double b = Double.parseDouble(inputBField.getText());
+                assemblerMessageLogArea.setText(assemblerMessageLogArea.getText()
+                        .concat("\nAnswer of dividing operation: ")
+                        .concat(String.valueOf(assemblerMath.getAssemblerFunctions().dividingFunction(a, b))));
+            } else {
+                if (variant.equals(choiceBoxVariants.getItems().get(1))) {
+                    int a = Integer.parseInt(inputAField.getText());
+                    int b = Integer.parseInt(inputBField.getText());
+                    assemblerMessageLogArea.setText(assemblerMessageLogArea.getText()
+                            .concat("\nAnswer of XOR operation: ")
+                            .concat(String.valueOf(assemblerMath.getAssemblerFunctions().xorFunction(a, b))));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            String message = e.getMessage();
+            assemblerMessageLogArea.setText(assemblerMessageLogArea.getText().concat("\n").concat(message));
+            showInformation(e.getCause().getClass().getSimpleName(), message, Alert.AlertType.ERROR);
+        }
     }
 }
