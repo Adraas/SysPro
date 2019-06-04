f392a2a9651685ab4ba36a24c77f73b9385a3783
Adraas <pickalov.artyom@yandex.ru>
2019-06-04 14:40:07 +0400

    fix: fix assembler inserts

--- jni-worker/src/main/c/ru/wkn/cnative/java_assembler_functions.cpp
+++ jni-worker/src/main/c/ru/wkn/cnative/java_assembler_functions.cpp
@@ -2,60 +2,55 @@
 #include <string.h>
 
 /*
- * Class:     ru_wkn_javasm_AssemblerFunctions
+ * Class:     ru_wkn_jni_functions_AssemblerFunctions
  * Method:    dividingFunction
  * Signature: (DD)D
  */
-extern "C" __declspec(dllexport) JNIEXPORT jdouble JNICALL Java_ru_wkn_javasm_AssemblerFunctions_dividingFunction
+extern "C" __declspec(dllexport) JNIEXPORT jdouble JNICALL Java_ru_wkn_jni_functions_AssemblerFunctions_dividingFunction
   (JNIEnv * jenv, jobject jobj, jdouble dividend, jdouble divisor)
 {
     double result;
     asm(
-        "fld %[dr]\n\t"
         "fld %[dd]\n\t"
-        "fdiv %%st(0), %%st(1)\n\t"
-        "fst %[res]\n\t"
+        "fdiv %[dr]\n\t"
+        "fstp %[res]\n\t"
         : [res] "=m" (result)
-        : [dr] "m" (divisor),
-        [dd] "m" (dividend)
+        : [dr] "m" (divisor), [dd] "m" (dividend)
         : "cc");
     return result;
 }
 
 /*
- * Class:     ru_wkn_javasm_AssemblerFunctions
+ * Class:     ru_wkn_jni_functions_AssemblerFunctions
  * Method:    xorFunction
  * Signature: (II)I
  */
-extern "C" __declspec(dllexport) JNIEXPORT jint JNICALL Java_ru_wkn_jni_AssemblerFunctions_xorFunction
+extern "C" __declspec(dllexport) JNIEXPORT jint JNICALL Java_ru_wkn_jni_functions_AssemblerFunctions_xorFunction
   (JNIEnv * jenv, jobject jobj, jint a, jint b)
 {
-    int result;
     asm(
-        "mov %%eax, %[a]\n\t"
-        "mov %%eax, %[b]\n\t"
-        "mov %[res], %%eax\n\t"
-        : [res] "=m" (result)
-        : [a] "m" (a),
-        [b] "m" (b)
+        "mov %[a], %%eax\n\t"
+        "xor %%eax, %[b]\n\t"
+        : "=m" (b)
+        : [a] "m" (a), [b] "m" (b)
         : "cc");
-    return result;
+    return b;
 }
 
 /*
- * Class:     ru_wkn_javasm_AssemblerFunctions
+ * Class:     ru_wkn_jni_functions_AssemblerFunctions
  * Method:    multiplicationOperation
  * Signature: (II)I
  */
-extern "C" __declspec(dllexport) JNIEXPORT jint JNICALL Java_ru_wkn_jni_AssemblerFunctions_multiplicationOperation
+extern "C" __declspec(dllexport) JNIEXPORT jint JNICALL Java_ru_wkn_jni_functions_AssemblerFunctions_multiplicationFunction
   (JNIEnv * jenv, jobject jobj, jint firstMultiplier, jint secondMultiplier)
 {
     int result;
     asm(
-        "mov %%ax, %[fm]\n\t"
-        "mov %%bx, %[sm]\n\t"
-        "mul %%bx\n\t"
-        "mov %[res], %%ax\n\t"
+        "mov %[fm], %%eax\n\t"
+        "mov %[sm], %%ebx\n\t"
+        "mul %%ebx\n\t"
+        "mov %%eax, %[res]\n\t"
         : [res] "=m" (result)
         : [fm] "m" (firstMultiplier),
         [sm] "m" (secondMultiplier)
