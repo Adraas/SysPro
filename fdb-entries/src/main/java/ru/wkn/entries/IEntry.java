6b74c33eafe2e23f72b42d9fca83122a6af709ed
Adraas <pickalov.artyom@yandex.ru>
2019-09-03 08:51:02 +0400

    feat: add new functional

--- fdb-entries/src/main/java/ru/wkn/entries/IEntry.java
+++ fdb-entries/src/main/java/ru/wkn/entries/IEntry.java
@@ -8,6 +8,20 @@ package ru.wkn.entries;
 public interface IEntry {
 
     /**
+     * The method for the obtaining an ID value of this {@code IEntry} object.
+     *
+     * @return an ID value
+     */
+    Long getId();
+
+    /**
+     * The method for the setting an ID value to this {@code IEntry} object.
+     *
+     * @param id a new given ID value
+     */
+    void setId(Long id);
+
+    /**
      * Method for the representation this entry as single {@code String} line.
      *
      * @return {@code String} line with main entry information
